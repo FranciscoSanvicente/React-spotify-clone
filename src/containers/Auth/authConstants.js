60339225a471d57108ca0fb98d9a37981a315e3f
Francisco Sanvicente Pico <64644109+FranciscoSanvicente@users.noreply.github.com>
2021-01-20 13:43:05 +0100

    Add files via upload

---
+++ src/containers/Auth/authConstants.js
@@ -0,0 +1,3 @@
+export const GET_USER_START = 'https://accounts.spotify.com/authorize';
+export const GET_USER_SUCCESS = '6f1f0de865d94aed83ecfffc96e9f89a';
+export const GET_USER_FAILURE = 'http://localhost:3000/';
